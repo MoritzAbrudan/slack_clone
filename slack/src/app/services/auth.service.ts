0c59ddee2a4f1461de4d4f4382c7696179bf06af
Sebastian Ebel <sebastianebel@hotmail.de>
2022-01-16 20:16:21 +0100

    login code cleaning

--- slack/src/app/services/auth.service.ts
+++ slack/src/app/services/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Auth } from '@angular/fire/auth';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { createUserWithEmailAndPassword} from 'firebase/auth';
 import { from } from 'rxjs';
 import { User } from 'src/models/user.class';
@@ -15,14 +17,42 @@ export class AuthService {
   user = new User();
   message: string = 'Login';
   guest: string = 'Guest';
-  login = true;
+  login = false;
 
-  constructor(public _auth: Auth, public firestore: AngularFirestore, public auth: AngularFireAuth) { }
+  constructor(public _auth: Auth, 
+    public router: Router, 
+    public firestore: AngularFirestore,
+    public msg: MatSnackBar, 
+    public auth: AngularFireAuth) { }
 
-  signInUser(userNames: string){
-    return userNames;
+    /**
+     * 
+     * login function
+     * @param userNames parameter for the username
+     * @param password parameter for the password
+     */
+  signInUser(userNames: string, password: string){
+    return this.firestore.collection('users').valueChanges({ idField: 'id' }).subscribe((result) => {
+      for (let i = 0; i < result.length; i++) {
+        if (result[i]['userName'] == userNames) {
+          this.login = true;
+          this.message = 'Logout';
+          this.router.navigateByUrl(`/slack/${result[i]['id']}`);
+        }
+      }
+    }, (error) => {
+      if (error.code === 'auth/wrong-password') this.msg.open('Wrong Password!', 'Try Again');
+      else if (error.code === 'auth/user-not-found') this.msg.open('User not found', 'Register please');
+      else this.msg.open(error, 'Close');
+    });
   }
 
+  /**
+   * 
+   * @param email for the email address
+   * @param password for the password
+   * @returns that will be return
+   */
   signUp(email: string, password: string){
     return from(createUserWithEmailAndPassword(this._auth, email, password).then(() =>{
       this.login = true;
@@ -30,6 +60,26 @@ export class AuthService {
     }));
   }
 
+  /**
+   * user can login as a guest
+   * 
+   */
+  guestLogin(){
+    this.firestore.collection('users').valueChanges({ idField: 'id' }).subscribe((result) => {
+      for (let i = 0; i < result.length; i++) {
+        if (result[i]['userName'] == this.guest) {
+          this.login = true;
+          this.message = 'Logout';
+          this.router.navigateByUrl(`/slack/${result[i]['id']}`);
+        }
+      }
+    });
+  }
+
+  /**
+   * 
+   * @param userId userId for the Slack Component you can show the username
+   */
   getUser(userId) {
     if (userId) {
       this.firestore
