8d3334449cddde7e7016c04679f3f6844d7b1376
Sebastian Ebel <sebastianebel@hotmail.de>
2022-01-25 20:17:51 +0100

    anwser if login wrong

--- slack/src/app/services/auth.service.ts
+++ slack/src/app/services/auth.service.ts
@@ -4,7 +4,7 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
-import { createUserWithEmailAndPassword} from 'firebase/auth';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { from } from 'rxjs';
 import { User } from 'src/models/user.class';
 
@@ -19,31 +19,35 @@ export class AuthService {
   guest: string = 'Guest';
   login = false;
 
-  constructor(public _auth: Auth, 
-    public router: Router, 
+  constructor(public _auth: Auth,
+    public router: Router,
     public firestore: AngularFirestore,
-    public msg: MatSnackBar, 
+    public msg: MatSnackBar,
     public auth: AngularFireAuth) { }
 
-    /**
-     * 
-     * login function
-     * @param userNames parameter for the username
-     * @param password parameter for the password
-     */
-  signInUser(userNames: string, password: string){
+  /**
+   * 
+   * login function
+   * @param userNames parameter for the username
+   * @param password parameter for the password
+   */
+  signInUser(userNames: string, password: string) {
     return this.firestore.collection('users').valueChanges({ idField: 'id' }).subscribe((result) => {
+
       for (let i = 0; i < result.length; i++) {
-        if (result[i]['userName'] == userNames) {
+        if ((result[i]['userName'] == userNames) && (result[i]['password'] == password)) {
           this.login = true;
           this.message = 'Logout';
           this.router.navigateByUrl(`/slack/${result[i]['id']}`);
+          break;
+        } else if (password != result[i]['password']) {
+          this.msg.open('Wrong Password!', 'Register please or Enter right Password');
+          break;
+        } else {
+          this.msg.open('User not found', 'Register please or Enter right Name');
+          break;
         }
       }
-    }, (error) => {
-      if (error.code === 'auth/wrong-password') this.msg.open('Wrong Password!', 'Try Again');
-      else if (error.code === 'auth/user-not-found') this.msg.open('User not found', 'Register please');
-      else this.msg.open(error, 'Close');
     });
   }
 
@@ -53,8 +57,8 @@ export class AuthService {
    * @param password for the password
    * @returns that will be return
    */
-  signUp(email: string, password: string){
-    return from(createUserWithEmailAndPassword(this._auth, email, password).then(() =>{
+  signUp(email: string, password: string) {
+    return from(createUserWithEmailAndPassword(this._auth, email, password).then(() => {
       this.login = true;
       this.message = 'Logout';
     }));
@@ -64,7 +68,7 @@ export class AuthService {
    * user can login as a guest
    * 
    */
-  guestLogin(){
+  guestLogin() {
     this.firestore.collection('users').valueChanges({ idField: 'id' }).subscribe((result) => {
       for (let i = 0; i < result.length; i++) {
         if (result[i]['userName'] == this.guest) {
